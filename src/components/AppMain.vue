68a80287dce7f70d99d75d33297439da617881b0
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-26 18:26:38 +0200

    imported all files in appMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,47 @@
 <template>
     <div>
-        
+        <AppHero/>
+        <GameCollection/>
+        <AppTeam/>
+        <GameMatches/>
+        <GameSquads/>
+        <AppCommunity/>
+        <AppStream/>
+        <AppMerch/>
+        <GamePartners/>
+        <AppPosts/>
+        <AppTestimonial/>
     </div>
 </template>
 
 <script>
+import AppHero from './AppHero.vue';
+import GameCollection from './GameCollection.vue';
+import AppTeam from './AppTeam.vue';
+import GameMatches from './GameMatches.vue';
+import GameSquads from './GameSquads.vue';
+import AppCommunity from './AppCommunity.vue';
+import AppStream from './AppStream.vue';
+import AppMerch from './AppMerch.vue';
+import GamePartners from './GamePartners.vue';
+import AppPosts from './AppPosts.vue';
+import AppTestimonial from './AppTestimonial.vue';
+
 export default {
-    name: "AppMain"
+    name: "AppMain",
+    
+    components: { AppHero,
+         GameCollection,
+        AppTeam,
+        GameMatches,
+        GameSquads, 
+        AppCommunity, 
+        AppStream, 
+        AppMerch, 
+        GamePartners, 
+        AppPosts, 
+        AppTestimonial
+    }
 }
 </script>
 
