3e711a83f0a6079da5bef6f5ef1c9b07cc255789
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-26 18:26:51 +0200

    Imported components in AppWrap

--- src/components/AppWrap.vue
+++ src/components/AppWrap.vue
@@ -1,12 +1,26 @@
 <template>
+
+import Appheader from './Appheader.vue';
     <div>
-        
+        <AppHeader/>
+        <AppMain/>
+        <AppFooter/>
     </div>
 </template>
 
 <script>
+import AppHeader from './Appheader.vue';
+import AppMain from './AppMain.vue';
+import AppFooter from './AppFooter.vue';
+
 export default {
-    name: "AppWrap"
+    name: "AppWrap",
+    
+    components: { 
+        AppHeader,
+        AppMain,
+        AppFooter,
+    }
 }
 </script>
 
