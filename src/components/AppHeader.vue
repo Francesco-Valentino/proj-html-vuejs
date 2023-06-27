e5630ad894f749cb3bf9e9c6a85a82df30afbbaa
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-27 12:08:49 +0200

    closed cols in row

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,29 +1,40 @@
 <template>
 
     <header>
-        <div class="container d-flex justify-content-between pt-3">
-            <div class="d-flex col-6 justify-content-between">
-                <p>
-                    <i class="fa-solid fa-phone"></i> +800-123-4567 6587
-                </p>
+        <div class="container pt-3">
+            <div class="row d-flex justify-content-between">
+                <div class="d-flex col-6 justify-content-between">
+                    <p>
+                        <i class="fa-solid fa-phone"></i> +800-123-4567 6587
+                    </p>
 
-                <p class="">
-                    <i class="fa-solid fa-location-dot"></i>  Beverley, New York 224 USA 
-                </p>
+                    <p class="">
+                        <i class="fa-solid fa-location-dot"></i>  Beverley, New York 224 USA 
+                    </p>
+                </div>
+
+                <div class="d-flex col-2 justify-content-between">
+                    <i class="fa-brands fa-facebook-messenger"></i>
+
+                    <i class="fa-brands fa-twitter"></i>
+
+                    <i class="fa-brands fa-vimeo-v"></i>
+
+                    <i class="fa-brands fa-skype"></i>
+
+                    <i class="fa-solid fa-rss"></i>
+                </div>
             </div>
+            
+        </div>
 
-            <div class="d-flex col-2 justify-content-between">
-                <i class="fa-brands fa-facebook-messenger"></i>
+        <div class="container">
+            <img :src="getImagePath(logoName)" alt="Gamer logo">
 
-                <i class="fa-brands fa-twitter"></i>
+            <nav>
 
-                <i class="fa-brands fa-vimeo-v"></i>
-
-                <i class="fa-brands fa-skype"></i>
-
-                <i class="fa-solid fa-rss"></i>
-            </div>
-    </div>
+            </nav>
+        </div>
     </header>
     
 
@@ -45,8 +56,16 @@ export default {
                 "Blog +",
                 "Contact",
             ],
+
+            logoName: "logo.png",
         }
    },
+
+   methods: {
+    getImagePath: function(img){
+        return new URL("../assets/img/$(img)", import.meta.url).href;
+    }
+   }
 }
 </script>
 
