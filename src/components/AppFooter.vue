164892656ff5b88825b61ff164d41704226b299e
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-28 17:37:10 +0200

    footer upper section started, incomplete

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,7 +6,7 @@
                     <div class="col-4 d-flex justify-content-center">
                         <img class="me-3" :src="getImagePath(contactImage)" alt="contact illustration">
 
-                        <p class="txt-white">
+                        <p class="txt-white mt-1">
                             Phone Number : +88012 345 678 912 
                         </p>
                     </div>
@@ -14,14 +14,14 @@
                     <div class="col-4 d-flex justify-content-center">
                         <img class="me-3" :src="getImagePath(contactImage)" alt="contact illustration">
 
-                        <p class="txt-white">
+                        <p class="txt-white mt-1">
                             Email : youremail@gmail.com                        </p>
                     </div>
 
                     <div class="col-4 d-flex justify-content-center">
                         <img class="me-3" :src="getImagePath(contactImage)" alt="contact illustration">
 
-                        <p class="txt-white">
+                        <p class="txt-white mt-1">
                             Address : 30 North West New York 240                         </p>
                     </div>
                 </div>
@@ -29,7 +29,9 @@
         </div>
 
         <div class="lowerFooter">
-
+            <p class="py-5 txt-white text-center">
+                ©2022 Gamer - eSpost And Gameing HTML Template.
+            </p>
         </div>
     </section>
 </template>
@@ -53,7 +55,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @use "../styles/partials/variables.scss" as *;
+
     div.upperFooter{
         background-image: url("../assets/img/bg\ \(3\).jpg");
+    }
+
+    div.lowerFooter{
+        background-color: $boxColor;
     }
 </style>
